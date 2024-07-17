154acdf26d1c4e35a8385cb1715350e363235e8f
Armando Vasquez <mando@mcnuggies.dev>
2024-07-17 14:38:27 -0500

    run models.Setup() in main

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 
+	"github.com/ax-vasquez/wedding-site-api/models"
 	"github.com/ax-vasquez/wedding-site-api/settings"
 )
 
 func main() {
 	settings.Setup()
+	models.Setup()
 	log.Println("HELLO")
 }
