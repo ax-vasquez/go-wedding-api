f81d90cc02b1155e64ffdcc7642a32f94e955e97
Armando Vasquez <mando@mcnuggies.dev>
2024-07-18 02:06:34 -0500

    remove unused type

--- settings/settings.go
+++ settings/settings.go
@@ -6,17 +6,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type Database struct {
-	Type        string
-	User        string
-	Password    string
-	Host        string
-	Name        string
-	TablePrefix string
-}
-
-var DatabaseSetting = &Database{}
-
 func Setup() {
 	err := godotenv.Load()
 	if err != nil {
