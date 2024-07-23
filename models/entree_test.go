0024c3f92253cc3440e0d816532c8ba121ef0f63
Armando Vasquez <mando@mcnuggies.dev>
2024-07-23 14:43:36 -0500

    initial entrees tests

--- models/entree_test.go
+++ models/entree_test.go
@@ -11,7 +11,15 @@ func TestEntrees(t *testing.T) {
 			t.Fatalf("Expected 5 entree records, received: %d", len(entrees))
 		}
 	})
+	// TODO: Investigate possible flake; this test failed with an index error 1-2 times, but passes all other times
 	t.Run("Can find entrees for user", func(t *testing.T) {
-
+		entrees := FindEntreesForUser(1)
+		if len(entrees) > 1 {
+			t.Fatalf("Users should only have 1 entree record; received: %d", len(entrees))
+		}
+		// All test users and invitees are set to the same initial entree selection
+		if entrees[0].OptionName != "Caprese pasta" {
+			t.Fatalf("Unexpected option selection found - expected \"Caprese pasta\", received: %s", entrees[0].OptionName)
+		}
 	})
 }
