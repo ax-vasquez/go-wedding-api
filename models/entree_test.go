f95a3b59b58871281622e0d170c532431d8dee92
Armando Vasquez <mando@mcnuggies.dev>
2024-07-23 14:28:25 -0500

    set all users and invitees to have the same initial entree/hors_doeuvres selection

--- models/entree_test.go
+++ models/entree_test.go
@@ -5,8 +5,13 @@ import (
 )
 
 func TestEntrees(t *testing.T) {
-	t.Run("Can get all entrees", func(t *testing.T) {
+	t.Run("Can find all possible entrees", func(t *testing.T) {
 		entrees := FindEntrees()
-		t.Log("DATA WOO: ", len(entrees))
+		if len(entrees) != 5 {
+			t.Fatalf("Expected 5 entree records, received: %d", len(entrees))
+		}
+	})
+	t.Run("Can find entrees for user", func(t *testing.T) {
+
 	})
 }
