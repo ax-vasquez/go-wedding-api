a346c4b31b8a5d8f4926478b1637ec7e2c6070de
Armando Vasquez <mando@mcnuggies.dev>
2024-07-29 13:42:02 -0500

    Don't panic when .env file isn't found; variables can exist without it

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	// Configure environment
 	err := godotenv.Load()
 	if err != nil {
-		log.Panic("Error loading .env file: ", err.Error())
+		log.Println("WARNING! Could not load .env file; application will continue to run with the assumption that needed variables are present in the environment.")
 	}
 	models.Setup()
 	models.Migrate()
